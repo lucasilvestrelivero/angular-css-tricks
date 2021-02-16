1b096106053ea13220ed484dcd7b96a96f6b1d00
Lucas Silvestre Livero-LSIL <lsil@motorola.com>
2021-02-16 19:23:13 -0400

    Correcting the navbar color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'galeria',
+    component: AppComponent
+  },
+  {
+    path: 'sobre',
+    component: AppComponent
+  },
+  {
+    path: 'contato',
+    component: AppComponent
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
